ec9ee113a3332bafa3c3e86b450bbecd98f14aa6
dschwilk <dschwilk@4b3877a5-4c1d-0410-b779-7f4527a406ac>
2007-09-03 23:32:39 +0000

    Updated setup.py
    git-svn-id: http://bibstuff.googlecode.com/svn/trunk@20 4b3877a5-4c1d-0410-b779-7f4527a406ac

--- setup.py
+++ setup.py
@@ -12,7 +12,5 @@ setup(name="bibstuff",
       url = "http://www.pricklysoft.org/software/bibstuff.html",
       license = "MIT",
       packages = ["bibstyles"],
-      py_modules=["bibgrammar", "bibfile", "bibname", "ebnf_sp"],
-      scripts=["bibsearch.py", "biblabel.py", "bib4txt.py", "jabbrev.py", "reflist.py", "add2bib.py"] )
-
- 
+      py_modules=["bibfile","bibgrammar", "bibname", "ebnf_sp"],
+      scripts=["add2bib.py", "bib4txt.py", "bibsearch.py", "biblabel.py", "jabbrev.py", "reflist.py"] )
