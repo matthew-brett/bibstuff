378f1e1e232c33a3fc8468a2bdb05d7476fa4477
Matthew Brett <matthew.brett@gmail.com>
2010-12-11 20:34:07 -0500

    RF - move make_sort_key into shared, give function interface to finding styles

--- bibstyles/__init__.py
+++ bibstyles/__init__.py
@@ -3,3 +3,15 @@
 This is the __init__.py for bibstyles package
 """
 
+from . import default
+from . import example_numbered
+from . import jasss_style
+
+_named = dict(
+    default = default,
+    example_numbered = example_numbered,
+    jasss_style = jasss_style,
+    jasss = jasss_style)
+
+def from_name(name):
+    return _named[name]
